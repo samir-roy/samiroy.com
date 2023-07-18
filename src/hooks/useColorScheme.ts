02ecc1c8932d0a961824187e0cee495ada316dff
Samir Roy <rsamir@gmail.com>
2023-07-18 15:33:35 +0530

    Fix SSR error - window not available

--- src/hooks/useColorScheme.ts
+++ src/hooks/useColorScheme.ts
@@ -1,15 +1,16 @@
 import * as React from 'react';
 
 type Theme = 'light' | 'dark';
-
-const query = window.matchMedia('(prefers-color-scheme: dark)');
-const match = (matches: boolean) => (matches ? 'dark' : 'light') as Theme;
+const convert = (matches: boolean) => (matches ? 'dark' : 'light') as Theme;
 
 const useColorScheme = () => {
-  const [theme, setTheme] = React.useState(match(query.matches));
+  const [theme, setTheme] = React.useState<Theme>('light');
 
   React.useEffect(() => {
-    const listener = (e: MediaQueryListEvent) => setTheme(match(e.matches));
+    const query = window.matchMedia('(prefers-color-scheme: dark)');
+    setTheme(convert(query.matches));
+
+    const listener = (e: MediaQueryListEvent) => setTheme(convert(e.matches));
     query.addEventListener('change', listener);
 
     return () => query.removeEventListener('change', listener);
