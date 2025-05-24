09d10570a9db713f92721709d34010d479cc5d73
Samir Roy <rsamir@gmail.com>
2025-05-24 08:30:14 +0530

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,7 @@
 import './src/static/styles/main.css';
 import { initializeTheme } from './src/utils/theme-utils';
 
-// Script to handle flash of unstyled content by setting theme before React hydrates
 export const onClientEntry = () => {
-  // On page load, immediately check localStorage and apply theme
-  // This prevents flickering when the user has a saved theme preference
   // eslint-disable-next-line no-eval
   eval(initializeTheme());
 };
