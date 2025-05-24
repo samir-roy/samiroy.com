27bcc469782dd6860e471d81f2654eaa272e19eb
Samir Roy <rsamir@gmail.com>
2025-05-24 08:29:24 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import './src/static/styles/main.css';
 import { initializeTheme } from './src/utils/theme-utils';
 
-// This adds a script to the <head> that sets the theme based on localStorage before page render
-// Prevents flash of incorrect theme
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <script
